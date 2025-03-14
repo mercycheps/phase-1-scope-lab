6503b41397774c537a4ac8995c39e2eca8948de6
Mercy <maritimmercy8@gmail.com>
2025-03-14 13:49:55 +0300

    Bob

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName='bob'.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe bob';
+}
+
+
